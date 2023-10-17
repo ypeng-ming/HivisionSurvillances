2dd44fdc7fad5a4365ac2020fc6f35f04ef736d9
ypeng-ming <52032205+ypeng-ming@users.noreply.github.com>
2023-10-17 08:16:42 +0800

    Add files via upload
    first

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include "mainwindow.h"
+#include <QTextCodec>
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
+#if _MSC_VER
+    QTextCodec *codec = QTextCodec::codecForName("gbk");
+#else
+    QTextCodec *codec = QTextCodec::codecForName("utf-8");
+#endif
+    QTextCodec::setCodecForLocale(codec);
+    QTextCodec::setCodecForCStrings(codec);
+    QTextCodec::setCodecForTr(codec);
+#else
+    QTextCodec *codec = QTextCodec::codecForName("utf-8");
+    QTextCodec::setCodecForLocale(codec);
+#endif
+    w.show();
+    return a.exec();
+}
